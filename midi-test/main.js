9734d95050d7435934152304423713dd0de0d552
Marlon Lückert <m.lueckert@me.com>
2019-11-12 14:36:38 +0100

    added tcp server, moved webinterface to port 8080

--- midi-test/main.js
+++ midi-test/main.js
@@ -2,21 +2,33 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const net = require('net');
+
+net.createServer(function(socket) {
+	socket.write('Echo server\r\n');
+    socket.pipe(socket);
+
+    // Add a 'data' event handler to this instance of socket
+    socket.on('data', function(data) {
+        console.log('DATA ' + socket.remoteAddress + ': ' + data);
+    });
+}).listen(3000, '141.22.88.158');
 
 // Webinterface in public
 app.use('/', express.static('public'))
 // serve socket.io clint from node_modules
 app.use('/scripts', express.static('node_modules/socket.io-client/dist/'))
-// start server at port 3000
-server.listen(3000)
+// start server at port 8080
+server.listen(8080)
 
-console.log("Webinterface available at http://localhost:3000/")
+console.log("Webinterface available at http://localhost:8080/")
 
 const MidiOutput = require('./midi.js');
 
 var midi = new MidiOutput();
 
 io.on('connection', client => {
+  console.log("connected");
   client.on('volume', ({value, channel}) => {
     midi.setVolume(value, channel)
    });
